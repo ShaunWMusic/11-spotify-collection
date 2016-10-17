c887d6327be47565da771b5d6612e2c0a899b39e
“Shaun <shaunwmusic@gmail.com>
2016-10-17 17:06:52 -0500

    update index.js

--- app/index.js
+++ app/index.js
@@ -1,18 +1,9 @@
 import 'whatwg-fetch';
 import tracks from './data';
 import model from './spotify-tracker/model-module';
-import view './spotify-tracker/view-module';
+import view from './spotify-tracker/view-module';
 
 
-tracks.forEach((data) => {
-  const c = new tracks(data);
-
-  const view = new view(c);
-// this runs the render in characterView
-  view.render();
-
-
-  document.body.appendChild(view.track);
-
-  console.log(c);
+data['tracks'].itemsforEach((track) => {
+  const c = new model(track);
 });
