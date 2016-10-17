2fb0e62d62455217f95b5f2bb5a7c6e461c8ce33
“Shaun <shaunwmusic@gmail.com>
2016-10-17 16:09:29 -0500

    Add anchor tag with href

--- app/spotify-tracker/view-module.js
+++ app/spotify-tracker/view-module.js
@@ -9,12 +9,15 @@ export default class view {
     this.track.classList.add('panel-block, track');
     this.track.innerHTML = `
       <div class="panel-block track">
+      <img src="" class="track__img" alt="">
+      <h2 class="track__artist"></h2>
+      <h4 class="track_name"><a class="track__link" href=""></a></h4>
 
 
       </div>`;
   }
   render() {
-// changed from a simple function.
+    // changed from a simple function.
     this.track.querySelector('.track__name').innerText = `${this.model.name}`;
     this.track.querySelector('.track__artist').innerText = `${this.model.artist}`;
     this.track.querySelector('.track__img').innerText = `${this.model.image}`;
