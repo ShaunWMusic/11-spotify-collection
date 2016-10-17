f571f76659f030b44644ec7629e871b625c3bedd
“Shaun <shaunwmusic@gmail.com>
2016-10-17 17:37:41 -0500

    Update index

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
 import 'whatwg-fetch';
 import data from './data';
-import model from './spotify-tracker/model';
-import view from './spotify-tracker/view';
+import Model from './spotify-tracker/model';
+import View from './spotify-tracker/view';
 
 console.log(data['tracks']);
 
+const results = document.querySelector('.results');
 data['tracks'].items.forEach((track) => {
-  const c = new model(track);
-  const t = new view(model);
-  debugger;
-  view.render();
+  const model = new Model(track);
+  const view = new View(model);
+
+// view.appendChild()
 });
